65e163f99ddc287ac862eacc51d7bc2feaf7a232
derekxiang <derekxiang@localhost>
2023-09-13 11:01:03 -0400

    fixed errors

--- DerekXiangNametag.groovy
+++ DerekXiangNametag.groovy
@@ -8,5 +8,5 @@ println "Width is"+width
 println "Height is"+height
 println "depth is"+depth
 
-CSG nameTagBase=newCube(width, height, depth).toCSG()
-return name TagBase
+CSG nameTagBase=new Cube(width, height, depth).toCSG()
+return nameTagBase
