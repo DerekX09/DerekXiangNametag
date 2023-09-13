2ffbc26ad9a3c68b4c7878561185f5ef7e16c699
derekxiang <derekxiang@localhost>
2023-09-13 10:59:41 -0400

    added name tag

--- DerekXiangNametag.groovy
+++ DerekXiangNametag.groovy
@@ -1,9 +1,12 @@
 // code here
 
-double width=30
+double width=60
 double depth=70
 double height=3
 
 println "Width is"+width
 println "Height is"+height
 println "depth is"+depth
+
+CSG nameTagBase=newCube(width, height, depth).toCSG()
+return name TagBase
